c56fc442ea6a2592097ca3ac5521a45ed72b1f95
Lee JaeHee <yikolden@naver.com>
2019-06-09 01:41:11 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-var gukbap = 3000
-var wonDom = null;
-var gukbapDom = null;
+let gukbap = 3000
+
+let wonDom = null;
+let gukbapDom = null;
+let gukbapBenchDom = null;
+
+let originGukbapMsg = null;
+let gukbapPriceMsgDom = null;
+
 
 
 function init() {
     wonDom = document.getElementById("origin_won");
     gukbapDom = document.getElementById("gukbap_count");
+    gukbapBenchDom = document.getElementById("gukbap_bench");
+
+    gukbapBenchDom.value = gukbap;
+
+    gukbapPriceMsgDom = document.getElementById("title_gukbap_price");
+    originGukbapMsg = gukbapPriceMsgDom.innerText;
+    change_msg()
 }
 
-function change_gukbap(params) {
-    alert(params)   
+function change_gukbap() {
+    gukbap = gukbapBenchDom.value;
+    change_msg()
 }
 
 function calculate_gukbap() {
-    alert(wonDom.value / gukbap + "국밥을 사먹을 수 있음")
+    let str = (wonDom.value / gukbap).toString();
+    gukbapDom.value = (str.substr(0,9))
+}
+
+function change_msg() {
+    gukbapPriceMsgDom.innerText = originGukbapMsg + " " + gukbap.toString() + "원"
 }
